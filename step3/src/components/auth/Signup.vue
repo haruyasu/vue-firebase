de201d93d9c82f4fa93d712efc97ae702bc27efa
haruyasu <harukun2002@gmail.com>
2020-04-28 21:03:33 +0900

    81 checking if an alias exists

---
+++ step3/src/components/auth/Signup.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="signup container">
+    <form class="card-panel" @submit.prevent="signup">
+      <h2 class="center deep-purple-text">Signup</h2>
+      <div class="field">
+        <label for="email">Email</label>
+        <input id="email" type="email" v-model="email" />
+      </div>
+      <div class="field">
+        <label for="password">Password</label>
+        <input id="password" type="password" v-model="password" />
+      </div>
+      <div class="field">
+        <label for="name">Alias</label>
+        <input id="name" type="text" v-model="alias" />
+      </div>
+      <p v-if="feedback" class="red-text center">{{ feedback }}</p>
+      <div class="field center">
+        <button class="btn deep-purple">Signup</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+import db from "@/firebase/init";
+import slugify from "slugify";
+export default {
+  name: "Signup",
+  data() {
+    return {
+      email: null,
+      password: null,
+      alias: null,
+      feedback: null,
+      slug: null
+    };
+  },
+  methods: {
+    signup() {
+      if (this.alias) {
+        this.slug = slugify(this.alias, {
+          replacement: "-",
+          remove: /[$*_+~.()'"!\-:@]/g,
+          lower: true
+        });
+        console.log(this.slug);
+        let ref = db.collection("users").doc(this.slug);
+        ref.get().then(doc => {
+          if (doc.exists) {
+            this.feedback = "This alias already exists";
+          } else {
+            // this alias does not yet exists in the db
+            this.feedback = "This alias is free to use";
+          }
+        });
+      } else {
+        this.feedback = "Please enter an alias";
+      }
+    }
+  }
+};
+</script>
+
+<style>
+.signup {
+  max-width: 400px;
+  margin-top: 60px;
+}
+.signup h2 {
+  font-size: 2.4em;
+}
+.signup .field {
+  margin-bottom: 16px;
+}
+</style>
